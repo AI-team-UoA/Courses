d4b8b089b860481f92ade245901a8815eff6246b
Nikoletos Konstantinos <47646955+Nikoletos-K@users.noreply.github.com>
2023-02-22 14:49:03 +0200

    added lab 1 - 5

---
+++ K08/Lab5/Exercise/bst.h
@@ -0,0 +1,29 @@
+#ifndef CODE_BST_H
+#define CODE_BST_H
+
+// Define a node in the binary search tree
+typedef struct BSTNode BSTNode;
+typedef struct BSTNode {
+    int data;
+    BSTNode *left, *right;
+} BSTNode;
+
+// Function pointer for visiting nodes during traversal
+typedef void (*Visit)(BSTNode* node);
+void visit(BSTNode* node);
+BSTNode* create_node(int data);
+BSTNode* insert(BSTNode* root, int data);
+
+/* -------------------------- */
+/* FUNCTIONS FOR LAB EXERCISE */
+/* -------------------------- */
+
+BSTNode* delete(BSTNode* root, int data);
+BSTNode* search(BSTNode* root, int data);
+void inorder(BSTNode* root, Visit visit);
+void preorder(BSTNode* root, Visit visit);
+void postorder(BSTNode* root, Visit visit);
+BSTNode* min_value(BSTNode* root);
+BSTNode* max_value(BSTNode* root);
+
+#endif //CODE_BST_H
---
+++ K08/Lab5/README.md
@@ -0,0 +1,3 @@
+Lab 5 - Binary search tree (BST)
+---
+
